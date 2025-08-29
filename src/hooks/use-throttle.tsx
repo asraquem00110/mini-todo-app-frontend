dcb21062e749b817cb109c6939eaa8fde9369c14
Alvin Raquem <araquem.git@gmail.com>
2025-08-30 02:26:26 +0800

    fix: temporary fix for use throttle hook

--- src/hooks/use-throttle.tsx
+++ src/hooks/use-throttle.tsx
@@ -1,16 +1,16 @@
 import { useRef, useEffect, useCallback } from 'react';
 import { throttle } from 'lodash';
 
-export const useThrottle = (cb: unknown, delayDuration: number) => {
+export const useThrottle = <T extends (...args: unknown[]) => unknown>(cb: T, delayDuration: number) => {
   const options = { leading: true, trailing: false }; // add custom lodash options
-  const cbRef = useRef(cb);
+  const cbRef = useRef<T>(cb);
 
   // use mutable ref to make useCallback/throttle not depend on `cb` dep
   useEffect(() => {
     cbRef.current = cb;
   });
   return useCallback(
-    throttle((...args: unknown) => cbRef.current(...args), delayDuration, options),
+    throttle((...args: Parameters<T>) => cbRef.current(...args), delayDuration, options),
     [delayDuration]
   );
 };
