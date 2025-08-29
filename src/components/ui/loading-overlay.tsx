68f7b8aa04142a2e0e61f47d472e5b10f5b3fdd8
Alvin Raquem <araquem.git@gmail.com>
2025-08-30 02:25:44 +0800

    fix: temporary fix for use throttle hook

--- src/components/ui/loading-overlay.tsx
+++ src/components/ui/loading-overlay.tsx
@@ -10,17 +10,12 @@ interface Props {
 }
 
 export const LoadingOverlay: React.FC<Props> = props => {
-  const { visible, classes = '', label = 'Processing ...' } = props;
+  const { visible, label = 'Processing ...' } = props;
 
   return (
     <>
       {visible && (
-        <div
-          className={cn(
-            'pin bg-graydark bg-opacity-50 fixed top-0 left-0 z-99999 flex h-screen w-screen overflow-auto',
-            classes
-          )}
-        >
+        <div className={cn('fixed inset-0 z-50 flex items-center justify-center bg-black/50')}>
           <div className="relative m-auto flex w-full max-w-md flex-col items-center rounded-md bg-white p-8">
             <Lottie loop animationData={BlueLoading} play style={{ width: 250, height: 250 }} />
             <h3 className="text-2xl text-blue-400">{label}</h3>
